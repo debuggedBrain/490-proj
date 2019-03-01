292f9b31cd13da13a6e04ff41459737a643fcee9
debuggedBrain <ah366@njit.edu>
2019-02-28 21:52:51 -0500

    authentication work

--- login.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-echo "Hello, how are you " + $username;
-?>
-</body>
-</html>
--- tester.php
+++ tester.php
@@ -1,9 +1,5 @@
-<html>
-<body>
-<h1>Hello there!</h1><br>
 <?php 
 $username = "matt\n";
 echo "just a test $username";
 ?>
-</body>
-</html>
+
