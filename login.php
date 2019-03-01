73abb98cba08260db6f483cb4c8965d86ea98d42
debuggedBrain <ah366@njit.edu>
2019-02-28 21:29:05 -0500

    auth work

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
+<html>
+<body>
 <?php
-
-echo "Hello " + $username;
-
+$username = $_POST['username'];
+$password = $_POST['password'];
+echo "Hello, how are you " + $username;
 ?>
+</body>
+</html>
