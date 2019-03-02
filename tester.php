0d0dda8938d3a9274a676ad6093d7b23bd3906a1
debuggedBrain <ah366@njit.edu>
2019-03-02 13:39:13 -0500

    testing

--- tester.php
+++ tester.php
@@ -1,6 +1,8 @@
 <?php
+require_once('rmq/testRabbitMQClient.php');
 $username1 = "matt\n";
-$username2 = $_POST["username3"];
+$username2 = "matt";
+//$username2 = $_POST["username3"];
 echo "just a test $username2";
 ?>
 
