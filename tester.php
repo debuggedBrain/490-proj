3361d1d393150bce326e33fa54e3dc5fce90a395
debuggedBrain <ah366@njit.edu>
2019-03-02 13:58:05 -0500

    trying to go directly from form to client

--- tester.php
+++ tester.php
@@ -1,5 +1,9 @@
 <?php
 require_once('rmq/testRabbitMQClient.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+
 $username1 = "matt\n";
 $username2 = "matt";
 //$username2 = $_POST["username3"];
